8d943d46de6a2c2870b4bc22196dae34aa8fe1ef
kubbar <kubbarr@hotmail.com>
2024-12-02 02:45:25 +0000

    ok

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const express = require('express');
+const cookieParser = require('cookie-parser');
 
 const app = express();
+app.use(cookieParser());
 
 app.use('/proxy', createProxyMiddleware({
   target: '',
   changeOrigin: true,
-  onProxyReq: (proxyReq, req) => {
+  onProxyReq: (proxyReq, req, res) => {
     const targetUrl = decodeURIComponent(req.query.target);
     proxyReq.setHeader('Referer', targetUrl);
     proxyReq.setHeader('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML، مثل Gecko) Chrome/131.0.0.0 Safari/537.36');
-    proxyReq.path = targetUrl;
+    
+    // نقل الكوكيز
+    if (req.headers.cookie) {
+      proxyReq.setHeader('cookie', req.headers.cookie);
+    }
+  },
+  onProxyRes: (proxyRes, req, res) => {
+    const cookies = proxyRes.headers['set-cookie'];
+    if (cookies) {
+      res.setHeader('set-cookie', cookies.map(cookie => cookie.replace(/; secure/gi, '')));
+    }
   },
   router: (req) => decodeURIComponent(req.query.target),
 }));
