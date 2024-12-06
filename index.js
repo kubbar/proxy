a75445f200464bab06d59f309ea8b58b45e63ad6
kubbar <kubbarr@hotmail.com>
2024-12-06 23:47:46 +0000

    ok

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
-const HttpsProxyAgent = require('https-proxy-agent');
+const HttpProxyAgent = require('http-proxy-agent');
 const app = express();
 
-const proxyUrl = '206.189.135.6:3128'; // استبدل هذا بعنوان البروكسي الخاص بك
-const agent = new HttpsProxyAgent(proxyUrl);
+const proxyUrl = 'http://206.189.135.6:3128'; // عنوان البروكسي الخاص بك
+const agent = new HttpProxyAgent(proxyUrl);
 
 app.use('/proxy', createProxyMiddleware({
   target: '',
@@ -13,7 +13,7 @@ app.use('/proxy', createProxyMiddleware({
   onProxyReq: (proxyReq, req) => {
     const targetUrl = decodeURIComponent(req.query.target);
     proxyReq.setHeader('Referer', targetUrl);
-    proxyReq.setHeader('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML، like Gecko) Chrome/131.0.0.0 Safari/537.36');
+    proxyReq.setHeader('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML، مثل Gecko) Chrome/131.0.0.0 Safari/537.36');
   },
   onProxyRes: (proxyRes, req, res) => {
     delete proxyRes.headers['x-frame-options'];
