7a1e2c6936ac125dc71db968c0bb4300e8829ad3
kubbar <kubbarr@hotmail.com>
2024-12-06 05:08:57 +0000

    ok

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use('/proxy', createProxyMiddleware({
   onProxyReq: (proxyReq, req, res) => {
     const targetUrl = decodeURIComponent(req.query.target);
     proxyReq.setHeader('Referer', targetUrl);
-    proxyReq.setHeader('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML، مثل Gecko) Chrome/131.0.0.0 Safari/537.36');
+    proxyReq.setHeader('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36');
   },
   onProxyRes: (proxyRes, req, res) => {
     // إزالة الرؤوس التي قد تسبب مشاكل في التحميل
